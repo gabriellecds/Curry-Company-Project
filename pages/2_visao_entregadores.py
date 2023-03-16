e379899a233d076d1d55351fa603ef2689439679
Gabrielle Oliveira <119449466+gabriellecds@users.noreply.github.com>
2023-03-16 15:09:08 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -89,7 +89,7 @@ def top_delivers(df1, top_asc):
 # ==============================================================================
 
 #import dataset
-df = pd.read_csv('dataset\\train.csv')
+df = pd.read_csv('dataset/train.csv')
 
 # Limpando os dados
 df1 = clean_code(df)
